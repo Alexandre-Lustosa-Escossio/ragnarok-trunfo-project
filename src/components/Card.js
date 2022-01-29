47f01011a64ecca94feb63adea7bbe6f8860dc5b
Alexandre <lutosaalexandre@gmail.com>
2022-01-28 21:31:28 -0300

    linter: Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Card extends React.Component {
   render() {
-    const { cardName, cardImage, cardDescription, cardAttr1, cardAttr2, cardAttr3, cardRare, cardTrunfo } = this.props;
+    const { cardName,
+      cardImage,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardRare,
+      cardTrunfo } = this.props;
     return (
       <div>
         <span data-testid="name-card">{cardName}</span>
@@ -17,5 +25,16 @@ class Card extends React.Component {
     );
   }
 }
+
+Card.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.number.isRequired,
+  cardAttr2: PropTypes.number.isRequired,
+  cardAttr3: PropTypes.number.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  cardTrunfo: PropTypes.bool.isRequired,
+};
 
 export default Card;
